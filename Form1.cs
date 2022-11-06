c4bf25dcb6c7e304b36d476fd1ce755019424123
VRBOX\GateKeeper <nikhil@doublestrike.com>
2022-11-06 16:06:09 -0500

    Initial - nonworking

---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using SharpDX.DirectInput;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JoystickVisualizer {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Initialize DirectInput
+            DirectInput directInput = new DirectInput();
+
+            // Find a Joystick Guid
+            var joystickGuid = Guid.Empty;
+
+            foreach (DeviceInstance deviceInstance in directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AllDevices))
+                joystickGuid = deviceInstance.InstanceGuid;
+
+            // If Joystick not found, throws an error
+            if (joystickGuid == Guid.Empty) {
+                Debug.WriteLine("No joystick/Gamepad found.");
+            }
+
+            // Instantiate the joystick
+            Joystick joystick = new Joystick(directInput, joystickGuid);
+
+            Debug.WriteLine("Found Joystick/Gamepad with GUID: {0}", joystickGuid);
+
+            // Query all suported ForceFeedback effects
+            var allEffects = joystick.GetEffects();
+            foreach (var effectInfo in allEffects)
+                Debug.WriteLine("Effect available {0}", effectInfo.Name);
+
+            // Set BufferSize in order to use buffered data.
+            joystick.Properties.BufferSize = 128;
+
+            // Acquire the joystick
+            joystick.Acquire();
+
+            // Poll events from joystick
+            while (true) {
+                joystick.Poll();
+                JoystickUpdate[] datas = joystick.GetBufferedData();
+                foreach (var state in datas)
+                    Debug.WriteLine(state);
+            }
+        }
+    }
+}
