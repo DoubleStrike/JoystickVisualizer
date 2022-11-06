daccc284d8f3b9168f0c52f12d6ff09a888231e1
VRBOX\GateKeeper <nikhil@doublestrike.com>
2022-11-06 16:30:51 -0500

    Cleaned up and returning real data!

--- Form1.cs
+++ Form1.cs
@@ -16,6 +16,8 @@ namespace JoystickVisualizer {
         private const string GLADIATOR_LEFT_NAME = " VKBsim Gladiator EVO  L  ";
         private const string GLADIATOR_RIGHT_NAME = " VKBsim Gladiator EVO  R  ";
 
+        private const int POLLING_SLEEP_MS = 100;
+
 
         public Form1() {
             InitializeComponent();
@@ -33,11 +35,10 @@ namespace JoystickVisualizer {
             bool joystickLFound = false;
             bool joystickRFound = false;
 
-            //foreach (DeviceInstance deviceInstance in directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AllDevices)) {
             foreach (DeviceInstance thisDevice in directInput.GetDevices(DeviceClass.GameControl, DeviceEnumerationFlags.AllDevices)) {
                 Debug.WriteLine($"Device instance found: '{thisDevice.InstanceName}'");
 
-                if(thisDevice.InstanceName == GLADIATOR_LEFT_NAME) {
+                if (thisDevice.InstanceName == GLADIATOR_LEFT_NAME) {
                     joystickLFound = true;
                     joystickLGuid = thisDevice.InstanceGuid;
                 } else if (thisDevice.InstanceName == GLADIATOR_RIGHT_NAME) {
@@ -61,31 +62,30 @@ namespace JoystickVisualizer {
             Joystick joystickR = new Joystick(directInput, joystickRGuid);
             Debug.WriteLine($"Found Right Joystick/Gamepad with GUID: '{joystickRGuid}'");
 
+            // Set BufferSize in order to use buffered data
+            joystickL.Properties.BufferSize = 128;
+            joystickR.Properties.BufferSize = 128;
+
+            // Acquire the joysticks
+            joystickL.Acquire();
+            joystickR.Acquire();
 
 
+            // Poll events from joystick
+            while (true) {
+                joystickL.Poll();
+                JoystickUpdate[] dataLeftStick = joystickL.GetBufferedData();
+                foreach (JoystickUpdate state in dataLeftStick)
+                    Debug.WriteLine($"Left: '{state}'");
 
+                joystickR.Poll();
+                JoystickUpdate[] dataRightStick = joystickR.GetBufferedData();
+                foreach (JoystickUpdate state in dataRightStick)
+                    Debug.WriteLine($"Right: '{state}'");
 
-
-
-
-            //// Query all suported ForceFeedback effects
-            //var allEffects = joystick.GetEffects();
-            //foreach (var effectInfo in allEffects)
-            //    Debug.WriteLine("Effect available {0}", effectInfo.Name);
-
-            //// Set BufferSize in order to use buffered data.
-            //joystick.Properties.BufferSize = 128;
-
-            //// Acquire the joystick
-            //joystick.Acquire();
-
-            //// Poll events from joystick
-            //while (true) {
-            //    joystick.Poll();
-            //    JoystickUpdate[] datas = joystick.GetBufferedData();
-            //    foreach (var state in datas)
-            //        Debug.WriteLine(state);
-            //}
+                // Sleep for the defined delay
+                System.Threading.Thread.Sleep(POLLING_SLEEP_MS);
+            }
         }
     }
 }
