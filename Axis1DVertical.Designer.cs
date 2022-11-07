f3f882c82db4bb38467be28262ea4585e7a3fba8
VRBOX\GateKeeper <nikhil@doublestrike.com>
2022-11-07 15:03:34 -0500

    I'm stupid - double buffering is broken everywhere

--- Axis1DVertical.Designer.cs
+++ Axis1DVertical.Designer.cs
@@ -1,7 +1,5 @@
-﻿namespace JoystickVisualizer
-{
-    partial class Axis1DVertical
-    {
+﻿namespace JoystickVisualizer {
+    partial class Axis1DVertical {
         /// <summary> 
         /// Required designer variable.
         /// </summary>
@@ -11,10 +9,8 @@
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -26,8 +22,7 @@
         /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
+        private void InitializeComponent() {
             this.SuspendLayout();
             // 
             // Axis1DVertical
@@ -35,7 +30,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CausesValidation = false;
-            this.DoubleBuffered = true;
             this.Name = "Axis1DVertical";
             this.Size = new System.Drawing.Size(40, 200);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Axis1D_Paint);
