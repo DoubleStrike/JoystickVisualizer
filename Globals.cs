ca6a19445be70bef3876b173baf8898f4994f91b
VRBOX\GateKeeper <nikhil@doublestrike.com>
2022-11-09 10:56:01 -0500

    Clean up unneeded usings

--- Globals.cs
+++ Globals.cs
@@ -1,12 +1,7 @@
 ﻿using SharpDX.DirectInput;
 using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace JoystickVisualizer {
     internal class Globals {
